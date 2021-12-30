f50ebec1814347c7b9d772ff37fdc792dc61b5e9
Oleksandr Kolomiiets <oleksandr.kolomiiets@diymaxeda.com>
2021-12-30 13:30:36 +0200

    Added tests for most frequently used scenarios

--- src/__tests__/delayless-lru-cache.test.js
+++ src/__tests__/delayless-lru-cache.test.js
@@ -19,4 +19,38 @@ describe('Delayless lru cache tests', () => {
     expect(task).toEqual(dummyTask)
     expect(errorHandler).toBeUndefined()
   })
+
+  test('Initial payload retrieving', async () => {
+    const dummyTask = async () => 'dummy response'
+    delaylessLruCache.setTaskOnce('test key', dummyTask)
+    const firstPromise = delaylessLruCache.get('test key')
+    const secondPromise = delaylessLruCache.get('test key')
+    expect(delaylessLruCache.runningTasks.size).toEqual(1)
+
+    const result = await Promise.all([firstPromise, secondPromise])
+    expect(result).toEqual(expect.arrayContaining(['dummy response', 'dummy response']))
+    expect(delaylessLruCache.runningTasks.size).toEqual(0)
+    expect(delaylessLruCache.cache.has('test key')).toBeTruthy()
+
+    const storedNode = delaylessLruCache.cache.get('test key')
+    expect(storedNode.value.payload).toEqual('dummy response')
+  })
+
+  test('Cached payload should be revalidated', async () => {
+    let counter = 0
+    const dummyTask = async () => {
+      counter++
+      return `dummy response ${counter}`
+    }
+    delaylessLruCache.setTaskOnce('test key', dummyTask)
+    const firstPayload = await delaylessLruCache.get('test key')
+    expect(firstPayload).toEqual('dummy response 1')
+    await setTimeout(3000)
+
+    const secondPayload = await delaylessLruCache.get('test key')
+    expect(secondPayload).toEqual('dummy response 1')
+
+    const thirdPayload = await delaylessLruCache.get('test key')
+    expect(thirdPayload).toEqual('dummy response 2')
+  })
 })
